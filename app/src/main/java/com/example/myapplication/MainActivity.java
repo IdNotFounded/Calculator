771e32578d3f2af4b6de92913a6a6fec1933c483
vanilnic <bikadog306@gmail.com>
2022-12-17 14:38:40 +0300

    changes

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+    private EditText form_number1, form_number2;
+    private Button add_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultTextView = findViewById(R.id.resultTextView);
+        form_number1 = findViewById(R.id.form_number1);
+        form_number2 = findViewById(R.id.form_number2);
+        add_button = findViewById(R.id.add_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float num1 = Float.parseFloat(form_number1.getText().toString());
+                float num2 = Float.parseFloat(form_number1.getText().toString());
+                float res = num1 + num2;
+                resultTextView.setText(String.valueOf(res));
+            }
+        });
+    }
+}
